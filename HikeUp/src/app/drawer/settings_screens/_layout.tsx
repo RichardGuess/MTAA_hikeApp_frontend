bc9f47cd9e90fc2be5842fadf0dbe252c83d2cea
Richard Gers <xgersr@stuba.sk>
2025-05-09 22:15:33 +0200

    better edit profile

--- HikeUp/src/app/drawer/settings_screens/_layout.tsx
+++ HikeUp/src/app/drawer/settings_screens/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { useThemeContext } from "../../../context/theme_context";
 import { ThemeProvider } from "@react-navigation/native";
+import { View } from "react-native";
 
 export default function SettingsScreensLayout() {
   const { theme } = useThemeContext();
@@ -12,6 +13,12 @@ export default function SettingsScreensLayout() {
           headerShown: true,
           headerTransparent: false,
           headerTitle: "Settings",
+          headerTitleAlign: "center", // centers the title
+          headerLeft: () => (
+            <View style={{ paddingLeft: 16 }}>
+              {/* Add your custom left header component here */}
+            </View>
+          ),
           headerStyle: {
             backgroundColor: theme.colors.card,
           },
