b274bbcc6164435b8e51839f06274c8e6f2ed583
adam <adamsintal3@gmail.com>
2025-05-16 21:17:37 +0200

    fix

--- HikeUp/src/assets/constants.ts
+++
@@ -1,6 +0,0 @@
-// export const LOCAL_IP = "http://147.175.162.24:5433";
-export const LOCAL_IP = "http://192.168.1.58:5433";
-// export const LOCAL_IP = "http://192.168.50.43:5433";
-// export const LOCAL_IP = "http://172.20.10.2:5433";
-export const GOOGLE_MAPS_API = "AIzaSyCc5P1g-iswEZZXPC-GixYAGkEtC5v-UR0";
-export const DIRECTIONS_API = "5b3ce3597851110001cf62481d62e427201141b8943d45c89915c402";
